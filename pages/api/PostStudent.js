b99d736753fd692e94ee0ece3e47b4238df12b5b
r0uven <roma-wolf2002@mail.ru>
2023-07-13 13:01:59 +0300

    Post corrected

--- pages/api/PostStudent.js
+++ pages/api/PostStudent.js
@@ -1,25 +1,25 @@
 import { kv } from '@vercel/kv';
-const fs = require('fs')
-
-
 
 export default async function handler(req, res) {
+    const data = req.body;
     if (req.method === 'POST') {
-        const data = req.body;
-        fs.readFile('Schet.txt', (err, id) => {
-            if (err) throw err;
-            console.log(id.toString());
-            fs.truncate('Schet.txt', 0, function () { console.log('done') })
-            fs.writeFile("Schet.txt", (parseInt(id,10) + 1).toString(), function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-                console.log("The file was saved!");
-            });
-            kv.hset(id.toString(), { id: id.toString(), name: data.studentName, group: data.groupName, faculty: data.facultyName, isDeleted: false });
-            res.status(200).json({ message: 'Data received!', data: data });
-        })
-
+        const user = []
+        let id
+        for await (const key of kv.scanIterator()) {
+            user.push(await kv.hgetall(key))
+        }
+        if (user.length == 0) {
+            id = 1
+        } else {
+            let j = 0
+            for (let i = 0; i < user.length; i++) {
+                if (user[i].id >= j)
+                    j = user[i].id
+            }
+            id = j+1
+        }
+        kv.hset(id.toString(), { id: id.toString(), name: data.studentName, group: data.groupName, faculty: data.facultyName, isDeleted: false });
+        res.status(200).json({ message: 'Data received!', data: data });
     } else {
         res.status(400).json({ message: 'Invalid request method!' });
     }
